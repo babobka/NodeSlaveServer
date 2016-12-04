3e953868f6431243d3ea0a3e6edd8f2bcc3605ec
babobka <babobka@bk.ru>
2016-12-04 19:35:28 +0300

    Fixed storage

--- src/main/java/ru/babobka/nodeslaveserver/model/TasksStorage.java
+++ src/main/java/ru/babobka/nodeslaveserver/model/TasksStorage.java
@@ -14,51 +14,36 @@ public class TasksStorage {
 	private final Map<Long, ConcurrentHashMap<Long, SubTask>> map = new ConcurrentHashMap<>();
 
 
-	/*
-	 * public static synchronized void put(Long taskId, Long requestId, SubTask
-	 * subTask) { if (map.get(taskId) == null) { map.put(taskId, new
-	 * ConcurrentHashMap<Long, SubTask>()); map.get(taskId).put(requestId,
-	 * subTask); } else { map.get(taskId).put(requestId, subTask); }
-	 * 
-	 * }
-	 */
 
-	public void put(NodeRequest request, SubTask subTask) {
+	public synchronized void put(NodeRequest request, SubTask subTask) {
 		if (!map.containsKey(request.getTaskId())) {
-			synchronized (TasksStorage.class) {
-				if (!map.containsKey(request.getTaskId())) {
-					map.put(request.getTaskId(), new ConcurrentHashMap<Long, SubTask>());
-					map.get(request.getTaskId()).put(request.getRequestId(), subTask);
-				}
-			}
+			map.put(request.getTaskId(), new ConcurrentHashMap<Long, SubTask>());
+			map.get(request.getTaskId()).put(request.getRequestId(), subTask);
+
 		} else {
 			map.get(request.getTaskId()).put(request.getRequestId(), subTask);
 		}
 
 	}
 
-	public boolean exists(Long taskId) {
+	public synchronized boolean exists(Long taskId) {
 		return map.containsKey(taskId);
 	}
 
-	public void removeRequest(NodeRequest request) {
+	public synchronized void removeRequest(NodeRequest request) {
 		ConcurrentHashMap<Long, SubTask> localTaskMap = map.get(request.getTaskId());
 		if (localTaskMap != null) {
 			localTaskMap.remove(request.getRequestId());
 			if (localTaskMap.isEmpty()) {
+
 				map.remove(request.getTaskId());
 			}
 		}
 	}
 
-	/*
-	 * public static void removeRequest(Long taskId, Long requestId) {
-	 * ConcurrentHashMap<Long, SubTask> localTaskMap = map.get(taskId); if
-	 * (localTaskMap != null) { localTaskMap.remove(requestId); if
-	 * (localTaskMap.isEmpty()) { map.remove(taskId); } } }
-	 */
 
-	public void stopTask(Long taskId) {
+
+	public synchronized void stopTask(Long taskId) {
 		ConcurrentHashMap<Long, SubTask> localTaskMap = map.get(taskId);
 		if (localTaskMap != null) {
 			for (ConcurrentHashMap.Entry<Long, SubTask> task : localTaskMap.entrySet()) {
@@ -69,7 +54,7 @@ public class TasksStorage {
 		map.remove(taskId);
 	}
 
-	public void stopAllTheTasks() {
+	public synchronized void stopAllTheTasks() {
 		for (Map.Entry<Long, ConcurrentHashMap<Long, SubTask>> taskEntry : map.entrySet()) {
 			stopTask(taskEntry.getKey());
 		}
